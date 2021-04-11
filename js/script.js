59c0cc56c66ac291c158666e8a32420eb7e62442
Olga <example@gmail.com>
2021-04-11 15:32:31 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//"use strict"; 
+const cat = 'toys';
+console.log(`https://someirl.com/` );
+
+let incr = 10,
+    decr = 10;
+//++incr;
+//--decr;
+console.log(++incr);
+console.log(--decr);
+
+const isCh = true,
+      isCl = true;
+console.log(isCh && isCl);
