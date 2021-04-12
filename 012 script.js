af3e8ad4abf3fb9a9202dc58a0979b5a5075baa9
Olga <example@gmail.com>
2021-04-12 13:22:34 +0300

    Olga

--- 012 script.js
+++ 012 script.js
@@ -23,7 +23,31 @@
 
 //'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+const numberOfFilms = prompt("Сколько фильмов вы уже смотрели?",'');
+
+const personalMovieDB = {
+        count : numberOfFilms,
+        movies : {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов','');
+
+const b = prompt('На сколько оцените его?','');
+
+const c = prompt('Один из последних просмотренных фильмов','');
+
+const d = prompt('На сколько оцените его?','');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
+
+
+/*const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
 
 const personalMovieDB = {
     count : numberOfFilms,
@@ -41,4 +65,4 @@ const a = prompt("Один из последних просмотренных ф
 personalMovieDB.movies[a] = b; 
 personalMovieDB.movies[c] = d;     
 
-console.log(personalMovieDB);
+console.log(personalMovieDB);*/
