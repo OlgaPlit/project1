b551b0628c19612b36ad5704cb00ee489587e23e
Olga <example@gmail.com>
2021-04-18 15:44:11 +0300

    Practik 24

--- 024 script.js
+++ 024 script.js
@@ -11,7 +11,101 @@
 Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
 при помощи метода forEach вывести в консоль сообщения в таком виде:
 "Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+ 
+(function () {'use strict';}());
 
-'use strict';
+const personalMovieDB = {
+        count : 0,
+        movies : {},
+        actors: {},
+        genres: [],
+        privat: false,
+        start : function()
+                {
+                    personalMovieDB.count = +prompt("Сколько фильмов вы уже смотрели?",'');
+                    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count))
+                    {
+                        personalMovieDB.count = +prompt("Сколько фильмов вы уже смотрели?",'');
+                    }
+                },
+        rememberMyFilms: function()
+                {
+                    for (let i=0; i<2; i++)//выполняем по тому кол-ву,сколько раз необходимо задать вопрос =2
+                    {
+                        const a = prompt('Один из последних просмотренных фильмов',''),
+                                b = prompt('На сколько оцените его?','');
+                                if (a != null && b != null && a!='' && b!='' && a.length <50 && b.length<50)
+                                {
+                                personalMovieDB.movies[a] = b;
+                                console.log('cool');
+                                }        
+                                else 
+                                {
+                                    console.log('error');
+                                    i--; 
+                                }
+                    }
+                },
+        detectPersonalLevel: function()
+                {
+                    if (personalMovieDB.count<10) 
+                    {
+                        console.log('мало фильмов');
+                    }
+                    else if (personalMovieDB.count >= 10 && personalMovieDB.count <30)
+                    {
+                        console.log('классика');
+                    }
+                    else if (personalMovieDB.count >= 30)
+                    { console.log('киноман');}
+                    else
+                    {console.log('ошибка');}
+                } ,  
+        showMyDB : function()
+                {
+                    if (personalMovieDB.privat == false)//возможен вариант !personalmovieDb
+                        {
+                        console.log(personalMovieDB);   
+                        }
+                }, 
+        writeYourGenres: function ()
+            {   /*let i = 0;
+                while (i < 3)// можно циклом for (let i=1, i<=3,i++)
+                //при цикле for запись в массив идет [i-1]
+                {
+                    //let vopr = prompt(`Ваш любимый жанр под номером ${i+1}`,"");
+                    let vopr1 = prompt(`Ваш любимый жанр под номером ${i+1}`,"");
+                    if (vopr1 == '' || vopr1 == null )
+                    {
+                        console.log('неверно')
+                        i--;
+                    }
+                    else {
+                    personalMovieDB.genres[i] = vopr1;                    
+                    i++;}
+                }
+                personalMovieDB.genres.forEach(function(item,i)
+                        {console.log(`Любимый жанр #${i+1} - это ${item}`);});
+                console.log(personalMovieDB);*/
+                let i = 0;
+                let genres = prompt("Введите жанры через запятую").toLowerCase();
+                if (genres == '' || genres == null )
+                    {
+                        console.log('неверно')
+                        i--;
+                    }
+                    else {
+                    personalMovieDB.genres=genres.split(', ');                    
+                    personalMovieDB.sort();}
+            },   
+                 
+        toggleVisibleMyDB: function()
+            {
+                if ( personalMovieDB.privat ) {personalMovieDB.privat = false;}
+                else {personalMovieDB.privat = true;
+                }
+            }       
+};
 
-// Код возьмите из предыдущего домашнего задания
+
+
